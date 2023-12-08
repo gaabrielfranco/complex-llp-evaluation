9eb175d55072769bf24129f3becfd800b7617751
gaabrielfranco <gabrielvsfranco@outlook.com>
2023-12-08 13:27:10 -0500

    LLP-VAT experiments

--- array_job_refit_gpu.sh
+++ array_job_refit_gpu.sh
@@ -3,7 +3,7 @@
 # Set SCC project
 
 # Submit an array job with 720 tasks
-#$ -t 1-3
+#$ -t 1-220
 
 # Specify hard time limit for the job.
 #   The job will be aborted if it runs longer than this time.
@@ -52,33 +52,9 @@ IFS=' ' # space is set as delimiter
 #     done
 # done
 
-# for dataset in "cifar-10-grey-animal-vehicle-hard-extra-extra-large-fol-clust-fol-clust-cluster-kmeans-autoencoder-40" "cifar-10-grey-animal-vehicle-hard-extra-large-fol-clust-fol-clust-cluster-kmeans-autoencoder-30" "cifar-10-grey-animal-vehicle-hard-large-equal-close-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-large-equal-far-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-large-equal-mixed-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-large-not-equal-close-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-large-not-equal-far-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-large-not-equal-mixed-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-massive-fol-clust-fol-clust-cluster-kmeans-autoencoder-50" "cifar-10-grey-animal-vehicle-hard-small-equal-close-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-small-equal-far-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-small-equal-mixed-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-small-not-equal-close-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-small-not-equal-far-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-small-not-equal-mixed-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-intermediate-extra-extra-large-fol-clust-fol-clust-cluster-kmeans-autoencoder-40" "cifar-10-grey-animal-vehicle-intermediate-extra-large-fol-clust-fol-clust-cluster-kmeans-autoencoder-30" "cifar-10-grey-animal-vehicle-intermediate-large-equal-close-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-intermediate-large-not-equal-close-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-intermediate-massive-fol-clust-fol-clust-cluster-kmeans-autoencoder-50" "cifar-10-grey-animal-vehicle-intermediate-small-equal-close-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-intermediate-small-not-equal-close-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-naive-extra-extra-large-fol-clust-None-cluster-kmeans-autoencoder-40" "cifar-10-grey-animal-vehicle-naive-extra-large-fol-clust-None-cluster-kmeans-autoencoder-30" "cifar-10-grey-animal-vehicle-naive-large-equal-None-cluster-None-None" "cifar-10-grey-animal-vehicle-naive-large-not-equal-None-cluster-None-None" "cifar-10-grey-animal-vehicle-naive-massive-fol-clust-None-cluster-kmeans-autoencoder-50" "cifar-10-grey-animal-vehicle-naive-small-equal-None-cluster-None-None" "cifar-10-grey-animal-vehicle-naive-small-not-equal-None-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-extra-extra-large-fol-clust-fol-clust-cluster-kmeans-autoencoder-40" "cifar-10-grey-animal-vehicle-simple-extra-large-fol-clust-fol-clust-cluster-kmeans-autoencoder-30" "cifar-10-grey-animal-vehicle-simple-large-equal-close-global-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-large-equal-far-global-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-large-equal-mixed-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-large-not-equal-close-global-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-large-not-equal-far-global-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-large-not-equal-mixed-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-massive-fol-clust-fol-clust-cluster-kmeans-autoencoder-50" "cifar-10-grey-animal-vehicle-simple-small-equal-close-global-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-small-equal-far-global-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-small-equal-mixed-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-small-not-equal-close-global-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-small-not-equal-far-global-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-small-not-equal-mixed-cluster-None-None"
-# do
-#     for model in "mixbag"
-#     do
-#         for loss in "abs"
-# 		do
-#             for n_split in "5"
-#             do
-#                 for splitter in "split-bag-shuffle"
-#                 do
-#                     for validation_size_perc in "0.5"
-#                     do
-#                         for ((exec=0; exec<5; exec++))
-#                         do
-#                             params[idx]=$dataset$IFS$model$IFS$loss$IFS$splitter$IFS$n_split$IFS$validation_size_perc$IFS$exec
-#                             ((idx++))
-#                         done
-#                     done
-#                 done
-#             done
-# 		done
-#     done
-# done
-
-for dataset in "cifar-10-grey-animal-vehicle-naive-small-not-equal-None-cluster-None-None"
+for dataset in "cifar-10-grey-animal-vehicle-hard-extra-extra-large-fol-clust-fol-clust-cluster-kmeans-autoencoder-40" "cifar-10-grey-animal-vehicle-hard-extra-large-fol-clust-fol-clust-cluster-kmeans-autoencoder-30" "cifar-10-grey-animal-vehicle-hard-large-equal-close-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-large-equal-far-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-large-equal-mixed-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-large-not-equal-close-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-large-not-equal-far-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-large-not-equal-mixed-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-massive-fol-clust-fol-clust-cluster-kmeans-autoencoder-50" "cifar-10-grey-animal-vehicle-hard-small-equal-close-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-small-equal-far-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-small-equal-mixed-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-small-not-equal-close-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-small-not-equal-far-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-hard-small-not-equal-mixed-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-intermediate-extra-extra-large-fol-clust-fol-clust-cluster-kmeans-autoencoder-40" "cifar-10-grey-animal-vehicle-intermediate-extra-large-fol-clust-fol-clust-cluster-kmeans-autoencoder-30" "cifar-10-grey-animal-vehicle-intermediate-large-equal-close-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-intermediate-large-not-equal-close-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-intermediate-massive-fol-clust-fol-clust-cluster-kmeans-autoencoder-50" "cifar-10-grey-animal-vehicle-intermediate-small-equal-close-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-intermediate-small-not-equal-close-global-cluster-kmeans-5" "cifar-10-grey-animal-vehicle-naive-extra-extra-large-fol-clust-None-cluster-kmeans-autoencoder-40" "cifar-10-grey-animal-vehicle-naive-extra-large-fol-clust-None-cluster-kmeans-autoencoder-30" "cifar-10-grey-animal-vehicle-naive-large-equal-None-cluster-None-None" "cifar-10-grey-animal-vehicle-naive-large-not-equal-None-cluster-None-None" "cifar-10-grey-animal-vehicle-naive-massive-fol-clust-None-cluster-kmeans-autoencoder-50" "cifar-10-grey-animal-vehicle-naive-small-equal-None-cluster-None-None" "cifar-10-grey-animal-vehicle-naive-small-not-equal-None-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-extra-extra-large-fol-clust-fol-clust-cluster-kmeans-autoencoder-40" "cifar-10-grey-animal-vehicle-simple-extra-large-fol-clust-fol-clust-cluster-kmeans-autoencoder-30" "cifar-10-grey-animal-vehicle-simple-large-equal-close-global-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-large-equal-far-global-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-large-equal-mixed-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-large-not-equal-close-global-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-large-not-equal-far-global-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-large-not-equal-mixed-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-massive-fol-clust-fol-clust-cluster-kmeans-autoencoder-50" "cifar-10-grey-animal-vehicle-simple-small-equal-close-global-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-small-equal-far-global-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-small-equal-mixed-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-small-not-equal-close-global-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-small-not-equal-far-global-cluster-None-None" "cifar-10-grey-animal-vehicle-simple-small-not-equal-mixed-cluster-None-None"
 do
-    for model in "mixbag"
+    for model in "llp-vat"
     do
         for loss in "abs"
 		do
@@ -88,55 +64,7 @@ do
                 do
                     for validation_size_perc in "0.5"
                     do
-                        for exec in "1"
-                        do
-                            params[idx]=$dataset$IFS$model$IFS$loss$IFS$splitter$IFS$n_split$IFS$validation_size_perc$IFS$exec
-                            ((idx++))
-                        done
-                    done
-                done
-            done
-		done
-    done
-done
-
-for dataset in "cifar-10-grey-animal-vehicle-simple-small-not-equal-close-global-cluster-None-None"
-do
-    for model in "mixbag"
-    do
-        for loss in "abs"
-		do
-            for n_split in "5"
-            do
-                for splitter in "split-bag-shuffle"
-                do
-                    for validation_size_perc in "0.5"
-                    do
-                        for exec in "0"
-                        do
-                            params[idx]=$dataset$IFS$model$IFS$loss$IFS$splitter$IFS$n_split$IFS$validation_size_perc$IFS$exec
-                            ((idx++))
-                        done
-                    done
-                done
-            done
-		done
-    done
-done
-
-for dataset in "cifar-10-grey-animal-vehicle-simple-small-not-equal-mixed-cluster-None-None"
-do
-    for model in "mixbag"
-    do
-        for loss in "abs"
-		do
-            for n_split in "5"
-            do
-                for splitter in "split-bag-shuffle"
-                do
-                    for validation_size_perc in "0.5"
-                    do
-                        for exec in "1"
+                        for ((exec=0; exec<5; exec++))
                         do
                             params[idx]=$dataset$IFS$model$IFS$loss$IFS$splitter$IFS$n_split$IFS$validation_size_perc$IFS$exec
                             ((idx++))
